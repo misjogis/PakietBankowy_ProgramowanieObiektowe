5b0402b41c23aa1d9e60c5565a81773faec92b40
Piotr Bobiński WSB <62461206+pbobinski-WSB@users.noreply.github.com>
2020-04-22 10:59:55 +0200

    podzadanie 3.2
    Podzadanie 3.2 – Tworzenie klienta banku

--- TestBanking.java
+++ TestBanking.java
@@ -9,11 +9,15 @@ import banking.*;
 public class TestBanking {
 
   public static void main(String[] args) {
+    Customer customer;
     Account  account;
 
     // Create an account that can has a 500.00 balance.
-    System.out.println("Creating an account with a 500.00 balance.");
-    account = new Account(500.00);
+    System.out.println("Creating the customer Jane Smith.");
+    customer = new Customer("Jane", "Smith");
+    System.out.println("Creating her account with a 500.00 balance.");
+    customer.setAccount(new Account(500.00));
+    account = customer.getAccount();
 
     System.out.println("Withdraw 150.00");
     account.withdraw(150.00);
@@ -25,6 +29,8 @@ public class TestBanking {
     account.withdraw(47.62);
 
     // Print out the final account balance
-    System.out.println("The account has a balance of " + account.getBalance());
+    System.out.println("Customer [" + customer.getLastName()
+		       + ", " + customer.getFirstName()
+		       + "] has a balance of " + account.getBalance());
   }
 }
